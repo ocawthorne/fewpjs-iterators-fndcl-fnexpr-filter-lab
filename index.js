7d4003b7615c32b3b3ef5b17bcc0610de7805fc3
ocawthorne <ocawthorne@gmail.com>
2021-01-20 17:52:51 +0000

    filter-lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, str) {
+   return drivers.filter(name => name.toLowerCase() === str.toLowerCase())
+}
+
+
+
+function fuzzyMatch(drivers, str) {
+   let fL = l => l.charAt(0).toLowerCase()
+   return drivers.filter(name => fL(name) === fL(str))
+}
+
+function matchName(drivers, str) {
+   return drivers.filter(drivObj => drivObj.name === str)
+}
